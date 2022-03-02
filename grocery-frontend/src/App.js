5395d648782a740d768d3d3915c326aa86864604
Lily Chen <chenlily1998@gmail.com>
2022-03-02 14:52:06 -0500

    added search bar

--- grocery-frontend/src/App.js
+++ grocery-frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import {render} from "react-dom";
@@ -6,6 +5,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Link
 } from "react-router-dom";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -13,10 +13,8 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import {useState, useEffect} from 'react';
 import Show from "./pages/Show"
-
-
-
-
+import CreateForm from "./pages/CreateForm"
+import EditForm from "./pages/EditForm"
 
 
 
@@ -29,129 +27,49 @@ const App = () => {
   const [newTag, setNewTag] = useState([])
   const [newDelivery, setNewDelivery] = useState(false)
   const [groceries, setGroceries] = useState([])
-
-
-
+  const [filter, setFilter] = useState('')
 
   useEffect(()=>{
   axios
-      .get('http://localhost:3000/groceries')
+      .get('https://stark-shelf-08940.herokuapp.com/groceries')
       .then((response)=>{
         setGroceries(response.data);
       })
   },[])
 
 
-
-const handleNewNameChange = (event)=>{
-  setNewName(event.target.value);
-}
-
-const handleNewDescriptionChange = (event)=>{
-  setNewDescription(event.target.value);
-}
-
-const handleNewStockChange = (event)=>{
-setNewStock(event.target.checked);
-}
-
-const handleNewDeliveryChange = (event)=>{
-setNewDelivery(event.target.checked);
-}
-
-const handleNewTagChange = (event)=>{
-setNewTag(event.target.value);
-}
-
-const handleNewImageChange = (event)=>{
-setNewImage(event.target.value);
-}
-const handleNewPriceChange = (event)=>{
-setNewPrice(event.target.value);
-}
-
-const handleNewGroceryFormSubmit = (event)=>{
-    event.preventDefault();
-    axios.post(
-        'http://localhost:3000/groceries',
-        {
-            name:newName,
-            image: newImage,
-            description: newDescription,
-            price: newPrice,
-            inStock:newStock,
-            delivery: newDelivery,
-            tag: newTag
-        }).then(()=>{
-        axios
-            .get('http://localhost:3000/groceries')
-            .then((response)=>{
-                setGroceries(response.data)
-            })
-        })
-  }
-
   const handleDelete = (groceryData)=>{
     axios
-        .delete(`http://localhost:3000/groceries/${groceryData._id}`)
+        .delete(`https://stark-shelf-08940.herokuapp.com/groceries/${groceryData._id}`)
         .then(()=>{
             axios
-                .get('http://localhost:3000/groceries')
+                .get('https://stark-shelf-08940.herokuapp.com/groceries')
                 .then((response)=>{
                     setGroceries(response.data)
                 })
         })
 }
 
-const handleToggleStock = (groceryData)=>{
-    axios
-        .put(
-            `http://localhost:3000/groceries/${groceryData._id}`,
-            {
-                name: groceryData.name,
-                image: groceryData.image,
-                description:groceryData.description,
-                price: groceryData.price,
-                inStock:!groceryData.inStock,
-                delivery: !groceryData.delivery,
-                tag: groceryData.tag
-            }
-        )
-        .then(()=>{
-            axios
-                .get('http://localhost:3000/groceries')
-                .then((response)=>{
-                    setGroceries(response.data)
-                })
-        })
-}
-
-const showItem = () => {
-
-}
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
   return (
-    <>
+      <>
 
     <main>
+      <div className = 'header'>
       <h1>Lily and Jihee GROCERY</h1>
+      <div className = 'searchBar'>
+      <input className = 'searchInput' type="text" placeholder="search..." value={filter} onChange={(e) => {e.preventDefault(); setFilter(e.target.value);
+          }}
+        ></input><br/>
+        </div>
+      </div>
       <section>
       <h2>Browse groceries</h2>
-      <ul className = 'groceryContainer'>
+
+      <div className = 'groceryContainer'>
           {
-              groceries.map((grocery)=>{
+              groceries.filter((search) =>
+        search.name.toLowerCase().includes(filter.toLowerCase())).map((grocery)=>{
                   return <div key = {grocery._id} >
                   <div className = 'groceryDiv'>
                   {<li className = 'groceryName'>{grocery.name}</li>}
@@ -159,46 +77,29 @@ const style = {
                   {<img src = {grocery.image} />}
 
 
-                  <Show name = {grocery.name} image = {grocery.image} />
-
-
-                  {<li className = 'descriptionLI'>{grocery.description}</li>}
-                  <div className = 'priceStock'>
-                  {<li>Price: {grocery.price}</li>}
-
-                  {grocery.inStock ? <li>In Stock</li> : <li>Out of Stock</li>}
-                  {<li>{grocery.tag}</li>}
-                  </div>
+                  <Show name = {grocery.name} image = {grocery.image}
+                        description = {grocery.description}
+                        tag = {grocery.tag}
+                        inStock = {grocery.inStock ? <li>In Stock</li> : <li>Out of Stock</li>}/>
 
               <button onClick={ (event)=>{ handleDelete(grocery) } }>Delete</button>
 
               <div>
 
               </div>
-                  </div>
+            </div>
 
-                  </div>
+          </div>
 
               })
           }
-      </ul>
-      <h2>Create a grocery</h2>
-      <form onSubmit={handleNewGroceryFormSubmit}>
-        Name: <input type = 'text' onChange={handleNewNameChange}/><br/>
-        Image URL: <input type = 'text' onChange={handleNewImageChange}/><br/>
-        Description: <input type = 'text' onChange={handleNewDescriptionChange}/><br/>
-        Price: <input type = 'text' onChange={handleNewPriceChange}/><br/>
-        In Stock: <input type = 'checkbox' onChange={handleNewStockChange}/><br/>
-        Available for Delivery: <input type = 'checkbox' onChange={handleNewDeliveryChange}/><br/>
-        <input type = 'submit' value = 'Add grocery' />
-        </form>
+      </div>
+      <CreateForm />
+      <EditForm/>
       </section>
     </main>
     </>
   )
 }
-
-
-
 
 export default App;
